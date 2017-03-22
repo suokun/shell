f3364ea5199faa9d4b2372f3b9cfbc473bc7b193
Tony <suokun24@126.com>
2017-03-22 12:17:50 -0500

    Update run-sockperf-latency-std-dev.sh

--- run-sockperf-latency-std-dev.sh
+++ run-sockperf-latency-std-dev.sh
@@ -23,21 +23,21 @@ do
 done
 
 
-echo "average latency"
+#echo "average latency"
 grep "sockperf: Summary: Latency" ./$var | awk ' { print ( $(NF-1) ) }' | awk '{sum+=$1}END{print sum/10 }'
 
-echo "max 99 latency"
-grep "sockperf: ---> percentile 99.000" ./$var | awk ' { print ( $(NF) ) }'  | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "Max=", max}'
+#echo "max 99 latency"
+grep "sockperf: ---> percentile 99.000" ./$var | awk ' { print ( $(NF) ) }'  | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "", max}'
 
-echo "max 99.9 latency"
-grep "sockperf: ---> percentile 99.900" ./$var | awk ' { print ( $(NF) ) }'  | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "Max=", max}'
+#echo "max 99.9 latency"
+grep "sockperf: ---> percentile 99.900" ./$var | awk ' { print ( $(NF) ) }'  | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "", max}'
 
-echo "max std-dev"
+#echo "max std-dev"
 grep "std-dev" ./$var | awk ' { print ( $(NF) ) }' > t0
 sed -n 's/^(std-dev=//p' t0 > t1
-sed -n 's/)//p' t1 | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "Max=", max}'
+sed -n 's/)//p' t1 | awk 'BEGIN {max = 0} {if ($1+0 > max+0) max=$1} END {print "", max}'
 
-echo "avg std-dev"
+#echo "avg std-dev"
 sed -n 's/)//p' t1 | awk '{sum+=$1}END{print sum/10 }'
 
 rm t0
