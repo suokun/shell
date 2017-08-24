1e8dd54e508a75aa65e74599d4cf5fff3e741855
Tony <suokun@users.noreply.github.com>
2017-08-24 10:25:20 -0500

    Create get-hardirq.py

---
+++ get-hardirq.py
@@ -0,0 +1,26 @@
+import os
+import time
+import commands
+
+os.system('sudo echo 0-3 > /proc/irq/11/smp_affinity_list')
+
+os.system("cat /proc/interrupts | grep 'CPU'")
+out1,str1=commands.getstatusoutput("cat /proc/interrupts | grep 'ens3' | awk '{print $2, $3, $4, $5}'")
+print str1
+
+
+
+time.sleep(5)
+
+
+
+out2,str2=commands.getstatusoutput("cat /proc/interrupts | grep 'ens3' | awk '{print $2, $3, $4, $5}'")
+print str2
+
+array1 = str1.split(" ")
+array2 = str2.split(" ")
+
+
+print " "
+for i in range(len(array1)):
+    print "CPU %d delta:%d"%(i, int(array2[i]) - int(array1[i]))
